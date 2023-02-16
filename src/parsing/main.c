fb406e30bcd78098e1da8ee254c709fb98d496be
Eguelin <eguelin@student.42lyon.fr>
2023-02-16 17:59:07 +0100

    main test

---
+++ src/parsing/main.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 15:30:44 by eguelin           #+#    #+#             */
+/*   Updated: 2023/02/16 16:27:17 by eguelin          ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+
+	fd = open("bible.txt", O_RDONLY);
+	ft_putnbr_fd(ft_count_line(fd), 1);
+	close(fd);
+	return (0);
+}
