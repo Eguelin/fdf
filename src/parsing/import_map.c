aafbea44bc06d87e3d6ab2e0b8856408cf0c8279
Eguelin <eguelin@student.42lyon.fr>
2023-02-20 16:51:21 +0100

    upgrade parsing

--- src/parsing/import_map.c
+++ src/parsing/import_map.c
@@ -6,18 +6,20 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 14:40:17 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/16 21:29:38 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/02/20 16:27:27 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	ft_count_line(int fd)
+int	ft_count_point(const char *path)
 {
 	char	buf[26];
 	int		i;
 	int		j;
+	int		fd;
 
+	fd = open(path, O_RDONLY);
 	ft_bzero(buf, 26);
 	j = 0;
 	while (read(fd, buf, 25))
@@ -25,11 +27,12 @@ int	ft_count_line(int fd)
 		i = 0;
 		while (buf[i])
 		{
-			if (buf[i] == '\n')
+			if (buf[i] == ' ' || buf[i] == '\n')
 				j++;
 			buf[i] = 0;
 			i++;
 		}
 	}
+	close(fd);
 	return (j);
 }
--- src/parsing/main.c
+++ src/parsing/main.c
@@ -6,20 +6,15 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:30:44 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/19 21:05:22 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/02/20 13:55:15 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <fcntl.h>
-#include <stdio.h>
 
-int	main(void)
+int	main(int argc, char const *argv[])
 {
-	int		fd;
-
-	fd = open("./maps/42.fdf", O_RDONLY);
-	ft_putnbr_fd(ft_count_line(fd), 1);
-	close(fd);
+	(void)argc;
+	ft_putnbr_fd(ft_count_line(argv[1]), 1);
 	return (0);
 }
