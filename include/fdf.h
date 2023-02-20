fd7dc4854f36f70083284a1352195ce5b68176ff
Eguelin <eguelin@student.42lyon.fr>
2023-02-20 13:32:40 +0100

    upgrade fdf.h

--- include/fdf.h
+++ include/fdf.h
@@ -6,13 +6,14 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:43:10 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/16 16:26:44 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/02/20 13:22:20 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
-# include "../lib/include/mylib.h"
+# include "../lib/mylib/include/mylib.h"
+# include <fcntl.h>
 
 int	ft_count_line(int fd);
 
