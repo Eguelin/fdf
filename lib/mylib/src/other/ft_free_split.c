afe8b4b80be393f3fba80b00987a1b324152a284
Eguelin <eguelin@student.42lyon.fr>
2023-02-21 17:38:56 +0100

    upgrade mylib

--- lib/mylib/src/other/ft_free_split.c
+++ lib/mylib/src/other/ft_free_split.c
@@ -6,9 +6,11 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/20 17:02:39 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/20 17:04:10 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/02/21 17:37:02 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "mylib.h"
 
 void	ft_free_split(char **tab)
 {
