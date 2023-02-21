c392fb2a2b6806e75298249beee503742c2ba060
Eguelin <eguelin@student.42lyon.fr>
2023-02-21 10:08:31 +0100

    upgrade parsing

--- src/parsing/import_map.c
+++ src/parsing/import_map.c
@@ -6,33 +6,35 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 14:40:17 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/20 16:27:27 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/02/21 10:07:01 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	ft_count_point(const char *path)
+void	ft_size_file(const char *path, t_data *data)
 {
 	char	buf[26];
+	char	c;
 	int		i;
-	int		j;
 	int		fd;
 
 	fd = open(path, O_RDONLY);
 	ft_bzero(buf, 26);
-	j = 0;
+	c = ' ';
 	while (read(fd, buf, 25))
 	{
 		i = 0;
 		while (buf[i])
 		{
-			if (buf[i] == ' ' || buf[i] == '\n')
-				j++;
+			if (c != ' ' && (buf[i] == ' ' || buf[i] == '\n') && data->y == 0)
+				data->x++;
+			if (buf[i] == '\n')
+				data->y++;
+			c = buf[i];
 			buf[i] = 0;
 			i++;
 		}
 	}
 	close(fd);
-	return (j);
 }
--- src/parsing/main.c
+++ src/parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 15:30:44 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/20 13:55:15 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/02/21 10:08:02 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	main(int argc, char const *argv[])
 {
+	t_data	data;
+
+	data.x = 0;
+	data.y = 0;
 	(void)argc;
-	ft_putnbr_fd(ft_count_line(argv[1]), 1);
+	ft_size_file(argv[1], &data);
+	ft_printf("x = %d y = %d", data.x, data.y);
 	return (0);
 }
