82c3d117f052605f9de4c893cb4f9f7ec956faf9
Eguelin <eguelin@student.42lyon.fr>
2023-03-01 20:08:25 +0100

    add key

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 13:43:10 by eguelin           #+#    #+#             */
-/*   Updated: 2023/02/28 19:29:06 by eguelin          ###   ########lyon.fr   */
+/*   Updated: 2023/03/01 19:11:35 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,84 @@
 # include <fcntl.h>
 # include <math.h>
 
-# ifndef RAD
-#  define RAD (3.141593 / 180)
-# endif
+# define RAD (3.141592653589793 / 180)
+
+# define ESC 65307
+# define F1 65470
+# define F2 65471
+# define F3 65472
+# define F4 65473
+# define F5 65474
+# define F6 65475
+# define F7 65476
+# define F8 65477
+# define F9 65478
+# define F10 65479
+# define F11 65480
+# define F12 65481
+# define GRAVE_ACCENT 96
+# define ONE 49
+# define TWO 50
+# define THREE 51
+# define FOUR 52
+# define FIVE 53
+# define SIX 54
+# define SEVEN 55
+# define EIGHT 56
+# define NINE 57
+# define ZERO 48
+# define DASH_UNDERSCORE 45
+# define EQUALS_PLUS 61
+# define BACKSPACE 65288
+# define TAB 65289
+# define Q 113
+# define W 119
+# define E 101
+# define R 114
+# define T 116
+# define Y 121
+# define U 117
+# define I 105
+# define O 111
+# define P 112
+# define OPEN_BRACKET 91
+# define CLOSE_BRACKET 93
+# define BACKSLASH 92
+# define CAPS_LOCK 65509
+# define A 97
+# define S 115
+# define D 100
+# define F 102
+# define G 103
+# define H 104
+# define J 106
+# define K 107
+# define L 108
+# define SEMICOLON 59
+# define SINGLE_QUOTE 39
+# define ENTER 65293
+# define LEFT_SHIFT 65505
+# define Z 122
+# define X 120
+# define C 99
+# define V 118
+# define B 98
+# define N 110
+# define M 109
+# define COMMA 44
+# define PERIOD 46
+# define SLASH 47
+# define RIGHT_SHIFT 65506
+# define LEFT_COMMAND 65507
+# define LEFT_ALT 65513
+# define SPACE 32
+# define RIGHT_ALT 65514
+# define MENU 65383
+# define RIGHT_COMMAND 65508
+# define UP_ARROW 65362
+# define LEFT_ARROW 65361
+# define DOWN_ARROW 65364
+# define RIGHT_ARROW 65363
 
 typedef struct s_image {
 	void	*img;
@@ -48,16 +123,19 @@ typedef struct s_data
 	int		y_max;
 	int		height;
 	int		length;
-	float	zoom;
-	int		mid_height;
-	int		mid_length;
+	float	intensity;
+	float	mid_height;
+	float	mid_length;
 	float	a;
 	float	b;
 	float	c;
+	float	zoom;
 
 }				t_data;
 
 void	ft_import_map(const char *path, t_data *data);
-void 	ft_rotation_matrix(t_data *data);
+void 	ft_projection(t_data *data);
+int		ft_image(t_data *data);
+int		ft_exit(t_data *data);
 
 #endif
