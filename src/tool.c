05c791e8d3f7e9088129a8abf412f7caf449ad5c
Eguelin <eguelin@student.42lyon.fr>
2023-03-03 20:33:16 +0100

    nothing

--- src/tool.c
+++ src/tool.c
@@ -3,27 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   tool.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emilien <emilien@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: eguelin <eguelin@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 17:21:30 by eguelin           #+#    #+#             */
-/*   Updated: 2023/03/03 00:02:51 by emilien          ###   ########lyon.fr   */
+/*   Updated: 2023/03/03 18:53:35 by eguelin          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	ft_init_fdf(t_data *data, const char *arg)
+void	ft_init_fdf(t_data *data, const char *path)
 {
 	data->x_max = 0;
 	data->y_max = 0;
-	ft_import_map(arg, data);
-	data->mid_height = HEIGHT / 2;
-	data->mid_length = LENGTH / 2;
-	data->a = PI / 4;
-	data->b = PI / 180 * -35;
-	data->c = PI / 6;
-	data->intensity = 1;
-	data->zoom = 2000 / data->x_max;
+	ft_import_map(path, data);
+	ft_isometric(data);
 	data->mlx = mlx_init();
 	if (!data->mlx)
 		exit(1);
